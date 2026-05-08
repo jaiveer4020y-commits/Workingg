9e28f6c7b0c0ce87e1811c54e6f9e699719d7c16
jaiveer4020y-commits <jaiveer4020y@gmail.com>
2026-05-08 17:33:12 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  experimental: { appDir: true },
+  experimental: { appDir: false },
   async headers() {
     return [
       {
